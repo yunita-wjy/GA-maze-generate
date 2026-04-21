3ce5062b0aee331f67f30a65c1f8f9d63b8d24fc
yunita-wjy <yunitawjy173@gmail.com>
2026-04-21 09:06:29 +0700

    add: chromosome.py

--- GA/chromosome.py
+++ GA/chromosome.py
@@ -1,6 +1,17 @@
 import random
+from config.settings import GRID_WIDTH, GRID_HEIGHT
 
 class Chromosome:
     def __init__(self, genes=None):
-        # genes = list of tuples
-        self.genes = genes if genes is not None else []
+        self.genes = genes if genes else self.generate_initial()
+
+    def generate_initial(self):
+        return [
+            [
+                random.randint(0, GRID_WIDTH-1),
+                random.randint(0, GRID_HEIGHT-1),
+                random.randint(5, 12),
+                random.randint(0, 1)
+            ]
+            for _ in range(random.randint(20, 30))
+        ]
